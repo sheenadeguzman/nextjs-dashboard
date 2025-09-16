478f59e73ab4d30ea2511bd4ef61d1cb237d4f6c
SheenaDeguzman <deguzmansheena30@gmail.com>
2025-09-16 19:54:36 +0800

    added sheena page

---
+++ app/Sheena/info.tsx
@@ -0,0 +1,6 @@
+export default function Info() {
+    return (
+        <p>I'm 20 years old</p>
+    )
+    
+}
---
+++ app/Sheena/page.tsx
@@ -0,0 +1,11 @@
+import Info from "./info"
+export default function Page() {
+    return (
+        <div>
+        <h1 className="text-3xl font-bold">Sheena De Guzman</h1>
+
+        <Info />
+        </div>
+
+    );
+}
